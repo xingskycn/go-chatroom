ff954d5f6f48ebb84c6f09160968ba23a02692ff
zhuo.meng <mengzhuo1203@gmail.com>
2014-12-18 23:47:15 +0800

    complete room

--- src/chat/room.go
+++ src/chat/room.go
@@ -6,29 +6,56 @@ import (
 	"log"
 )
 
+func kickName(msg *Message) string {
+	return fmt.Sprintf("%s", msg.Message)
+}
+
 type Room struct {
-	clients   map[string]*Client
-	Incomming chan *Message
+	Name    string
+	clients map[string]*Client
+	In      chan *Message
+	Quit    chan bool
 }
 
 func (r *Room) Listen() {
 
+	log.Printf("Chatroom: ", r.Name, " opened")
 	for {
 		select {
-		case msg := <-r.Incomming:
-			log.Printf(fmt.Sprintf("%s", msg.Message))
+		case msg := <-r.In:
 			switch msg.Command {
 			case QUIT:
 				delete(r.clients, msg.Sender.Name)
+				go r.broadcast(msg)
 			case JOIN:
 				r.clients[msg.Sender.Name] = msg.Sender
+				go r.broadcast(msg)
+			case KICK:
+				name := kickName(msg)
+				if val, ok := r.clients[name]; ok {
+					delete(r.clients, name)
+					go r.broadcast(msg)
+				}
+			case DISMISS:
+				// Blocking broadcasting...
+				r.broadcast(msg)
+				r.Quit <- true
+			default:
+				go r.broadcast(msg)
 			}
-			go r.Broadcast(msg)
+
+		case <-r.Quit:
+			for k := range r.clients {
+				delete(r.clients, k)
+			}
+			log.Printf("Chatroom: ", r.Name, " closed")
+			// Ok, Mission Completed
+			return
 		}
 	}
 }
 
-func (r *Room) Broadcast(msg *Message) {
+func (r *Room) broadcast(msg *Message) {
 
 	for _, c := range r.clients {
 		c.Incomming <- msg
